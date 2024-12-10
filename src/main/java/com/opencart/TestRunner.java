6df90504a802431b9a9d10c20945f707dc0682fb
Mihai <potoracmihai74@gmail.com>
2024-12-10 09:41:51 +0200

    Revert "Revert "Implement the Driver Manager and configure the Selenium dependencies""
    This reverts commit e7444a27e9eb2499cc1f1199c193c98d323ca03f.

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,7 +1,29 @@
 package com.opencart;
 
+import com.opencart.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
+import java.sql.Driver;
+
 public class TestRunner {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = DriverManager.getInstance().getDriver();
+        driver.get("https://google.com/");
+
+        String theNameOfTheFirstTab = driver.getWindowHandle();
+
+        //Open a new tab
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://diez.md/");
+        Thread.sleep(2000);
+        //Close the new tab
+        driver.close();
+
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://stiri.md/");
+        Thread.sleep(2000);
+        //Close the new tab
+        driver.close();
     }
 }
